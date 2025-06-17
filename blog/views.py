1917324a47faa620b8d8b809df6135221512914c
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-06-17 16:08:56 +0500

    fix def post_detail

--- blog/views.py
+++ blog/views.py
@@ -63,7 +63,7 @@ def post_detail(request, slug):
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': post.likes_count,
+        'likes_amount': post.likes__count,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
